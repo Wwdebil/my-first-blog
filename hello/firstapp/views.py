3803675500811f63103858be7e95d5c517f2053e
wwdebil <yarikmalcev096@gmail.com>
2022-02-28 21:28:35 +0600

    Last try to fix this site

---
+++ hello/firstapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+
+
+def index(request):
+    return HttpResponse("Index")
+
+
+def about(request):
+    return HttpResponse("About")
+
+
+def contact(request):
+    return HttpResponseRedirect("/about")
+
+
+def details(request):
+    return HttpResponsePermanentRedirect("/")
